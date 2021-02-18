5102359ff40fcce020a62a9d58583cf503cc98c4
Charly Mannion <charlottemannion100@gmail.com>
2021-02-18 15:37:34 +0000

    requests the data and writes it to an md file

---
+++ index.js
@@ -0,0 +1,52 @@
+const https = require("https");
+const fs = require("fs");
+
+// Making a request to an API and receiving the response. We are the client.
+
+const markdownGenerator = (contents, callback) => {
+  fs.writeFile(`./README.md`, `${contents}`, (err, readMeData) => {
+    if (err) callback(err);
+    else callback(null, readMeData);
+  });
+};
+
+const getInstructions = () => {
+  const options = {
+    hostname: "nc-leaks.herokuapp.com",
+    path: "/api/confidential",
+    method: "GET",
+  };
+
+  // setup a new request object with the options above
+  const request = https.request(options, (response) => {
+    // callback invoked with a response object
+
+    // declare a body variable to build the response from the packets
+    let body = "";
+
+    // use response's on method to run a callback every time a packet is received
+    response.on("data", (packet) => {
+      body += packet.toString();
+    });
+
+    response.on("end", () => {
+      //parse json string back into object ready to be used...
+      const parsedBody = JSON.parse(body);
+      //   console.log(parsedBody, "PARSED BODY");
+
+      // convert cryptic string to a readable one
+      const readableString = parsedBody.crypticString;
+      //   console.log(readableString, "PARSED TO READABLE STRING");
+
+      markdownGenerator(readableString, (err, readMeData) => {
+        if (err) callback(err);
+        else console.log("readme file has been made");
+      });
+    });
+  });
+
+  // call the end method to actually send the request once setup
+  request.end();
+};
+
+getInstructions();
